20817cb7f4397348dae386f183464d1405f13abc
Orlando Melo <nando.adventista@hotmail.com>
2020-04-27 16:33:48 -0300

    Todos os 8 controllers criados.

---
+++ GerenciadorDeEquipamentos/GerenciadorDeEquipamentos/Controllers/TipoSolicitacaoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GerenciadorDeEquipamentos.Controllers
+{
+    public class TipoSolicitacaoController : Controller
+    {
+        // GET: TipoSolicitacao
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
